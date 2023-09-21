c098ee16991d416cfe219e648a015809b2cd5ec1
Shameer Abdul Kader <62144206+shameer896@users.noreply.github.com>
2023-09-21 17:24:24 +0530

    Create Merge 2 Sorted Array.java
    Given two sorted arrays, arr1, and arr2 of size n and m. Find the union of two sorted arrays.
    
    The union of two arrays can be defined as the common and distinct elements in the two arrays.NOTE: Elements in the union should be in ascending order.
    
    Examples
    Example 1:
    Input:
    n = 5,m = 5.
    arr1[] = {1,2,3,4,5}  
    arr2[] = {2,3,4,4,5}
    Output:
     {1,2,3,4,5}
    
    Explanation: 
    Common Elements in arr1 and arr2  are:  2,3,4,5
    Distnict Elements in arr1 are : 1
    Distnict Elemennts in arr2 are : No distinct elements.
    Union of arr1 and arr2 is {1,2,3,4,5} 
    
    Example 2:
    Input:
    n = 10,m = 7.
    arr1[] = {1,2,3,4,5,6,7,8,9,10}
    arr2[] = {2,3,4,4,5,11,12}
    Output: {1,2,3,4,5,6,7,8,9,10,11,12}
    Explanation: 
    Common Elements in arr1 and arr2  are:  2,3,4,5
    Distnict Elements in arr1 are : 1,6,7,8,9,10
    Distnict Elemennts in arr2 are : 11,12
    Union of arr1 and arr2 is {1,2,3,4,5,6,7,8,9,10,11,12}

---
+++ Merge 2 Sorted Array.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Solution {
+    public static List< Integer > sortedArray(int []arr1, int []arr2) {
+        // Write your code here
+        int i = 0, j = 0;
+        int n=arr1.length;
+        int m=arr2.length; // pointers
+  ArrayList<Integer > Union=new ArrayList<>(); // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
+        Union.add(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
+        Union.add(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.get(Union.size()-1) != arr1[i])
+      Union.add(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.get(Union.size()-1) != arr2[j])
+      Union.add(arr2[j]);
+    j++;
+  }
+  return Union;
+    }
+}
