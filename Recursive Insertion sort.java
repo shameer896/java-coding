8c6bffe88aa8ba9efd9422312299cb234ba60d34
Shameer Abdul Kader <62144206+shameer896@users.noreply.github.com>
2023-09-25 19:04:47 +0530

    Create Recursive Insertion sort.java

---
+++ Recursive Insertion sort.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public static void insertionSort(int[] arr, int size) {
+        //Your code goes here
+        for(int i=1;i<size;i++)
+        {
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j])
+            {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+}
