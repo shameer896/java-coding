34030f330db0aa1ed96836d6f2ded9eee4e7430f
Shameer Abdul Kader <62144206+shameer896@users.noreply.github.com>
2023-11-24 14:02:09 +0530

    Create Check Prime.java

---
+++ Check Prime.java
@@ -0,0 +1,15 @@
+public class Solution {
+	public static String isPrime(int num) {
+		//Your code goes here
+		if(num<=1)
+		return "NO";
+
+		for(int i=2;i<=(int)Math.sqrt(num);i++)
+		{
+			if(num%i==0)
+			return "NO";
+		}
+		
+		return "YES";
+	}
+}
